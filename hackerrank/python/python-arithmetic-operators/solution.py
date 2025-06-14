2c483110e34c2f09a97305883cc62f59416af7b0
nsurname2038 <214315071+nsurname2038@users.noreply.github.com>
2025-06-14 12:32:54 +0300

    feat(hackerrank): add solution to `python-arithmetic-operators`

---
+++ hackerrank/python/python-arithmetic-operators/solution.py
@@ -0,0 +1,10 @@
+# Author: @nsurname2038
+# Problem: https://www.hackerrank.com/challenges/python-arithmetic-operators
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    print(a + b)
+    print(a - b)
+    print(a * b)
