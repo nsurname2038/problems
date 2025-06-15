ac665095d2bb25a163a97ddb53d3c9de99427f09
nsurname2038 <214315071+nsurname2038@users.noreply.github.com>
2025-06-15 12:25:42 +0300

    feat(hackerrank): add solution to `python-division`

---
+++ hackerrank/python/python-division/solution.py
@@ -0,0 +1,9 @@
+# Author: @nsurname2038
+# Problem: https://www.hackerrank.com/challenges/python-division
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    print(a // b)
+    print(a / b)
